171c497750ebd14dd65ee736f25e4b0d8665bc91
Adam Kotler <kotler@bu.edu>
2020-11-15 18:10:25 -0800

    Cleaned up code; nothing major

--- turbine.m
+++ turbine.m
@@ -8,7 +8,7 @@ function[P_05,T_05,c_p5,gamma_5]=turbine(P_04,T_04,T_03,T_02,n_t,n_m,c_p4,gamma_
         T_av=(T_05+T_04)./2;
         [c_pnew,gamma_new]=cp(T_av,Rs);
         gamma_diff=(abs(gamma_new-gamma_temp))./gamma_temp;
-        fprintf("%f %f %f\n",T_05,gamma_new,gamma_diff);
+%         fprintf("%f %f %f\n",T_05,gamma_new,gamma_diff);
         gamma_temp=gamma_new;
         c_ptemp=c_pnew;
     end
--- turbomain.m
+++ turbomain.m
@@ -25,27 +25,18 @@ gamma_a=1.4;
 
 if(strcmp(str,"Ramjet"))
     noz_bool=input("Select Nozzle: 'C' 'C+D' ",'s');
+    %Perform Calculations for Strictly converging, choked nozzle
+    [P_0a,P_02,T_0a,T_02]=diffuser(P_a,T_a,gamma_a,M,pi_d);
+    [f,P_03,P_04,c_p3,c_p4,c_pav,gamma_3,gamma_4]=burner(P_02,T_02,T_04,pi_b,n_b,dh,Rs);
+    [P_06,P_star,T_06,V_e,c_p6,gamma_6]=nozzle(P_04,P_a,T_04,n_n,Rs,noz_bool);
+    %Output results for strictly converging nozzle
+    header={'MATLAB/CLASS','P_0a[Pa]','T_0a[K]','f[-]','P*[Pa]','V_exit[m/s]'};
+    mat_dat={'MATLAB',P_0a,T_0a,f,P_star,V_e};
+    class_dat={'CLASS',792,518,.0315,299,1108};
+        dat=vertcat(mat_dat,class_dat);
     if(strcmp(noz_bool,"C"))
-        %Perform Calculations for Strictly converging, choked nozzle
-        [P_0a,P_02,T_0a,T_02]=diffuser(P_a,T_a,gamma_a,M,pi_d);
-        [f,P_03,P_04,c_p3,c_p4,c_pav,gamma_3,gamma_4]=burner(P_02,T_02,T_04,pi_b,n_b,dh,Rs);
-        [P_06,P_star,T_06,V_e,c_p6,gamma_6]=nozzle(P_04,P_a,T_04,n_n,Rs,noz_bool);
-        %Output results for strictly converging nozzle
-        header={'MATLAB/CLASS','P_0a[Pa]','T_0a[K]','f[-]','P*[Pa]','V_exit[m/s]'};
-        mat_dat={'MATLAB',P_0a,T_0a,f,P_star,V_e};
-        class_dat={'CLASS',792,518,.0315,299,1108};
-            dat=vertcat(mat_dat,class_dat);
         CHOKED_TABLE=cell2table(dat,'VariableNames',header)
     else
-        %Perform Calculations for Strictly converging, choked nozzle
-        [P_0a,P_02,T_0a,T_02]=diffuser(P_a,T_a,gamma_a,M,pi_d);
-        [f,P_03,P_04,c_p3,c_p4,c_pav,gamma_3,gamma_4]=burner(P_02,T_02,T_04,pi_b,n_b,dh,Rs);
-        [P_06,P_star,T_06,V_e,c_p6,gamma_6]=nozzle(P_04,P_a,T_04,n_n,Rs,noz_bool);
-        %Output results for strictly converging nozzle
-        header={'MATLAB/CLASS','P_0a[Pa]','T_0a[K]','f[-]','P*[Pa]','V_exit[m/s]'};
-        mat_dat={'MATLAB',P_0a,T_0a,f,P_star,V_e};
-        class_dat={'CLASS',792,518,.0315,299,1108};
-            dat=vertcat(mat_dat,class_dat);
         CONVERGING_DIVERGING_TABLE=cell2table(dat,'VariableNames',header)
     end
 else
@@ -56,11 +47,11 @@ else
         [P_05,T_05,c_p5,gamma_5]=turbine(P_04,T_04,T_03,T_02,n_t,n_m,c_p4,gamma_4,c_pc,gamma_c,f,Rs);
         [P_06,P_star,T_06,T_star,V_e,c_p6,c_pn,gamma_6,gamma_n,A_ma,F_ma,TSFC,V_in]=nozzle(M,P_05,P_a,T_05,T_a,n_n,gamma_5,c_p5,f,Rs,noz_bool);
         %Output results
-        header={'MATLAB/CLASS','T_02[K]','P_02[Pa]','gamma_a[-]','T_03[K]','P_03[Pa]','c_pc[','f[-]','P*[Pa]','V_exit[m/s]'};
-        labels={'CLASS','MATLAB'};
-        mat_dat={'MATLAB',P_0a,T_0a,f,P_star,V_e};
-        class_dat={'CLASS',792,518,.0315,299,1108};
-            dat=vertcat(mat_dat,class_dat);
-        CONVERGING_DIVERGING_TABLE=cell2table(dat,'VariableNames',header)
+%         header={'MATLAB/CLASS','T_02[K]','P_02[Pa]','gamma_a[-]','T_03[K]','P_03[Pa]','c_pc[','f[-]','P*[Pa]','V_exit[m/s]'};
+%         labels={'CLASS','MATLAB'};
+%         mat_dat={'MATLAB',P_0a,T_0a,f,P_star,V_e};
+%         class_dat={'CLASS',792,518,.0315,299,1108};
+%             dat=vertcat(mat_dat,class_dat);
+%         CONVERGING_TABLE=cell2table(dat,'VariableNames',header);
 end
  
